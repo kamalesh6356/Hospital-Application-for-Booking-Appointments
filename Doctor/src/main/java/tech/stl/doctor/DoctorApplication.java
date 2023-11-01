3c82cec67b1ff7adef69bc25073c50e8e9b97d7f
kamalesh6356 <kamaleshamudala6356@gmail.com>
2023-11-01 23:11:19 +0530

    firstcommit

---
+++ Doctor/src/main/java/tech/stl/doctor/DoctorApplication.java
@@ -0,0 +1,22 @@
+package tech.stl.doctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class DoctorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate()
+	{
+		 return new RestTemplate();
+		
+		
+	}
+}
---
+++ Doctor/src/main/java/tech/stl/doctor/exception/DoctorNotFoundException.java
@@ -0,0 +1,10 @@
+package tech.stl.doctor.exception;
+
+public class DoctorNotFoundException extends Exception {
+
+	public DoctorNotFoundException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+}
